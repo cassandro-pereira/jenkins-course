15b0386d20ef422e6db1d4443b35d176e6bb9102
cassandro-pereira <112014056+cassandro-pereira@users.noreply.github.com>
2024-05-02 13:32:29 -0300

    Update nodejsdocker.groovy
    adjust dockerhub path

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('cassandro1010/docker-test-jenkins')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
